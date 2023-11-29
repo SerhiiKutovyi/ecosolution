edbe1ad92f7266d3c60ac3acde57b1a4791ba67d
SerhiiKutovyi <109694632+SerhiiKutovyi@users.noreply.github.com>
2023-11-29 22:40:40 +0200

    gitignore

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,5 @@
 const App = () => {
-  const myStyle = {
-    fontFamily: 'Fira Sans',
-    fontWeight: 400,
-  };
-  return <h1 style={myStyle}>New project</h1>;
+  return <h1>New project</h1>;
 };
 
 export { App };
