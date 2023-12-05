40bfafc555cb6faef652a2212d5402990425b096
SerhiiKutovyi <109694632+SerhiiKutovyi@users.noreply.github.com>
2023-12-05 18:36:26 +0200

    optimization the application

---
+++ src/components/App/App.jsx
@@ -0,0 +1,27 @@
+import About from '../About/About';
+import Cases from '../Cases/Cases';
+import ContactUs from '../ContactUs/ContactUs';
+import Electricity from '../Electricity/Electricity';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import Main from '../Main/Main';
+import Faq from '../Faq/Faq';
+
+import {Container} from './App.styled';
+
+const App = () => {
+  return (
+    <Container>
+      <Header />
+      <Main />
+      <About />
+      <Electricity />
+      <Cases />
+      <Faq />
+      <ContactUs />
+      <Footer />
+    </Container>
+  );
+};
+
+export default App;
---
+++ src/components/App/App.styled.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin-right: auto;
+  margin-left: auto;
+
+  width: 360px;
+
+  padding-left: 20px;
+  padding-right: 20px;
+
+  background-color: var(--layout-background-color);
+
+  @media screen and (min-width: 480px) {
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 768px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    width: 1280px;
+  }
+`;
